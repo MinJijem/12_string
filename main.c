8400fc664c7aaedd9f21519415b2dddc5edc5e64
MinJijem <55428081+MinJijem@users.noreply.github.com>
2019-11-19 15:35:12 +0900

    practice4

--- main.c
+++ main.c
@@ -6,13 +6,11 @@
 
 int main(int argc, char *argv[]) {
 	
-	char src[]="The worst things to eat before you sleep ";
-	char dst[100];
+	char str[30]="Happy C programming";
 	
+
 	
-	strcpy(dst,src);
-	
-	printf("copied string : %s",dst);
+	printf("문자열 \"%s\"의 길이 : %i",str,strlen(str));
 
 	
 	return 0;
